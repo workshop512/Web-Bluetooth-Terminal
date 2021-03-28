4794c2f050ce312c9195a14ae0b69e8d7c0d7644
workshop512 <wrkshp512@gmail.com>
2021-03-27 19:46:39 -0500

    Update BluetoothTerminal.js

--- js/BluetoothTerminal.js
+++ js/BluetoothTerminal.js
@@ -11,9 +11,10 @@ class BluetoothTerminal {
   
     constructor(serviceUuid = 6E400001-B5A3-F393-E0A9-E50E24DCCA9E, characteristicUuid = 6E400003-B5A3-F393-E0A9-E50E24DCCA9E,
       receiveSeparator = '\n', sendSeparator = '\n') {
-  
-  */
   constructor(serviceUuid = 0xFFE0, characteristicUuid = 0xFFE1,
+      receiveSeparator = '\n', sendSeparator = '\n') {  
+  */
+    constructor(serviceUuid = 6E400001-B5A3-F393-E0A9-E50E24DCCA9E, characteristicUuid = 6E400003-B5A3-F393-E0A9-E50E24DCCA9E,
       receiveSeparator = '\n', sendSeparator = '\n') {
     // Used private variables.
     this._receiveBuffer = ''; // Buffer containing not separated data.
